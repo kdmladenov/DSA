fab879d95d35b5169513999f5d2bd2fdaf12f501
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-12-03 20:43:07 -0500

    141. Linked List Cycle

---
+++ Linked Lists/141. Linked List Cycle.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+const hasCycle = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
