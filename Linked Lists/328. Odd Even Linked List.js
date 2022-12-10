d78c21864907d5318a569245795ec39e3ee2021b
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-12-09 19:15:52 -0500

    328. Odd Even Linked List

---
+++ Linked Lists/328. Odd Even Linked List.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/odd-even-linked-list/
+
+const oddEvenList = (head) => {
+  if (head === null) return null;
+
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+
+  while (even !== null && even.next !== null) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+  return head;
+};
