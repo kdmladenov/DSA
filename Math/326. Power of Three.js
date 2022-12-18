2ee4245871fb338a102eaa440c87df22ca73996a
Krasimir Mladenov <kdmladenov@yahoo.com>
2022-12-18 17:10:19 -0500

    326. Power of Three

---
+++ Math/326. Power of Three.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/power-of-three/
+
+// https://www.youtube.com/watch?v=1WJBV4hNodc
+
+const isPowerOfThree = (n) => {
+  if (n === 0) return false;
+
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+
+  return n === 1;
+};
