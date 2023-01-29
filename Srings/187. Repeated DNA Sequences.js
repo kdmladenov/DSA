43fcf3eee75509dc7f11c16c6f1af672458744ff
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-01-29 20:11:53 +0200

    187. Repeated DNA Sequences

---
+++ Srings/187. Repeated DNA Sequences.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/repeated-dna-sequences/
+
+const findRepeatedDnaSequences = (s) => {
+  let seen = new Set();
+  let result = new Set();
+
+  for (let i = 0; i < s.length - 9; i++) {
+    const str = s.slice(i, i + 10);
+
+    seen.has(str) ? result.add(str) : seen.add(str);
+  }
+
+  return Array.from(result);
+};
