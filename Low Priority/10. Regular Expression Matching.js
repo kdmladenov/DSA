7b2906288d84644d55f1b4133430af58a0c072c4
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-02-08 23:08:44 +0200

    10. Regular Expression Matching

---
+++ Low Priority/10. Regular Expression Matching.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/regular-expression-matching/description/?q=javascript&orderBy=most_votes
+
+const isMatch = (string, pattern) => {
+  let indexes = new Set();
+
+  const addStat = (indexes, i) => {
+    if (pattern[i] === '*') {
+      indexes.add(i - 1);
+      addStat(indexes, i + 1);
+    } else {
+      indexes.add(i);
+      if (pattern[i + 1] === '*') addStat(indexes, i + 2);
+    }
+  };
+
+  addStat(indexes, 0);
+
+  for (let char of string) {
+    let next = new Set();
+    for (let i of indexes) {
+      if (char === pattern[i] || pattern[i] === '.') addStat(next, i + 1);
+    }
+    indexes = next;
+  }
+  return indexes.has(pattern.length);
+};
