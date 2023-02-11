cd630e92fb3cee273e2d28caac099104c7373f4c
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-02-11 20:51:02 +0200

    350. Intersection of Two Arrays II

--- Arrays/350. Intersection of Two Arrays II.js
+++ Arrays/350. Intersection of Two Arrays II.js
@@ -1,24 +1,18 @@
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
-
 const intersect = (nums1, nums2) => {
   let res = [];
   let map = {};
 
-  // to make the map
-  for (let i = 0; i < nums1.length; i++) {
-    let curr = nums1[i];
-
-    map[curr] = map[curr] + 1 || 1;
+  for (let num1 of nums1) {
+    map[num1] = map[num1] + 1 || 1;
   }
 
   // to decrease the map count if found
-  for (let i = 0; i < nums2.length; i++) {
-    let curr = nums2[i];
-
-    if (map[curr]) {
-      res.push(curr);
-      map[curr] -= 1;
+  for (let num2 of nums2) {
+    if (map[num2]) {
+      res.push(num2);
+      map[num2]--;
     }
   }
 
