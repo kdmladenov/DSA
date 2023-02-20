4e66351b68118edcf0590f1ddbca485d972e4d6f
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-02-20 22:55:32 +0200

    494. Target Sum

---
+++ Dinamic Programming/494. Target Sum.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/target-sum/
+
+const findTargetSumWays = (nums, target) => {
+  const memo = new Map();
+
+  const dfs = (i, remain) => {
+    const key = `${i}#${remain}`;
+    
+    if (i < 0) return remain === 0 ? 1 : 0;
+    if (memo.has(key)) return memo.get(key);
+
+    const count = dfs(i - 1, remain + nums[i]) + dfs(i - 1, remain - nums[i]);
+
+    memo.set(key, count);
+
+    return count;
+  };
+
+  return dfs(nums.length - 1, target);
+};
