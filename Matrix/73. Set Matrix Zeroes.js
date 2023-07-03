2a1efd37755be96c79d387dcb2fdf9bfe00c1685
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-07-03 18:51:37 -0400

    73. Set Matrix Zeroes -code refactored

--- Matrix/73. Set Matrix Zeroes.js
+++ Matrix/73. Set Matrix Zeroes.js
@@ -1,53 +1,18 @@
 // https://leetcode.com/problems/set-matrix-zeroes/
 
 const setZeroes = (matrix) => {
-  let firstRowHasZero = false;
-  let firstColHasZero = false;
+  const map = new Set();
+  let rows = matrix.length;
+  let cols = matrix[0].length;
 
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-
-  //check if first row has zero
-  for (let i = 0; i < cols; i++) {
-    if (matrix[0][i] === 0) {
-      firstRowHasZero = true;
-      break;
-    }
-  }
-  //check if first col has zero
-  for (let i = 0; i < rows; i++) {
-    if (matrix[i][0] === 0) {
-      firstColHasZero = true;
-      break;
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (matrix[r][c] === 0) map.add([r, c]);
     }
   }
 
-  //check for zeros inside the matrix excluding first row and col
-  for (let r = 1; r < rows; r++) {
-    for (let c = 1; c < cols; c++) {
-      if (matrix[r][c] === 0) {
-        matrix[0][c] = 0;
-        matrix[r][0] = 0;
-      }
-    }
+  for (let [row, col] of map) {
+    for (let c = 0; c < cols; c++) matrix[row][c] = 0; // update row
+    for (let r = 0; r < rows; r++) matrix[r][col] = 0; // update column
   }
-
-  // zero out all (less first) rows and cols where there is zero
-  for (let r = 1; r < rows; r++) {
-    for (let c = 1; c < cols; c++) {
-      if (matrix[0][c] === 0 || matrix[r][0] === 0) matrix[r][c] = 0;
-    }
-  }
-
-  // zero out first row
-  if (firstRowHasZero) {
-    for (let c = 0; c < cols; c++) matrix[0][c] = 0;
-  }
-
-  // zero out first col
-  if (firstColHasZero) {
-    for (let r = 0; r < rows; r++) matrix[r][0] = 0;
-  }
-
-  return matrix;
 };
