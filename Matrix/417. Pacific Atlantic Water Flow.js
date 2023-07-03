51da50efa22c86a44731a3ba380bfb92b407eaec
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-07-02 20:36:55 -0400

    417. Pacific Atlantic Water Flow

--- Matrix/417. Pacific Atlantic Water Flow.js
+++ Matrix/417. Pacific Atlantic Water Flow.js
@@ -1,61 +1,45 @@
 // https://leetcode.com/problems/pacific-atlantic-water-flow/
 
-// https://leetcode.com/problems/pacific-atlantic-water-flow/discuss/589401/JavaScript-DFS
+const pacificAtlantic = (heights) => {
+  let rows = heights.length;
+  let cols = heights[0].length;
+  let pacMap = new Map();
+  let atlMap = new Map();
+  let res = [];
 
-const pacificAtlantic = (matrix) => {
-  if (matrix.length === 0) return [];
-
-  const pacific = [];
-  const atlantic = [];
-  const result = [];
-
-  for (let i = 0; i < matrix.length; i++) {
-    pacific[i] = Array(matrix[0].length).fill(0);
-    atlantic[i] = Array(matrix[0].length).fill(0);
-  }
-
-  // top and botom
-  for (let i = 0; i < matrix[0].length; i++) {
-    dfs(matrix, 0, i, Number.MIN_SAFE_INTEGER, pacific);
-    dfs(matrix, matrix.length - 1, i, Number.MIN_SAFE_INTEGER, atlantic);
-  }
-
-  // left and right
-  for (let i = 0; i < matrix.length; i++) {
-    dfs(matrix, i, 0, Number.MIN_SAFE_INTEGER, pacific);
-    dfs(matrix, i, matrix[0].length - 1, Number.MIN_SAFE_INTEGER, atlantic);
-  }
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (pacific[i][j] === 1 && atlantic[i][j] === 1) {
-        result.push([i, j]);
-      }
+  const dfs = (r, c, visit, prevHeight) => {
+    if (
+      r < 0 ||
+      c < 0 ||
+      r >= rows ||
+      c >= cols ||
+      heights[r][c] < prevHeight ||
+      visit.has(`${r}-${c}`)
+    ) {
+      return;
     }
+
+    visit.set(`${r}-${c}`, [r, c]);
+
+    dfs(r + 1, c, visit, heights[r][c]); 
+    dfs(r - 1, c, visit, heights[r][c]);
+    dfs(r, c + 1, visit, heights[r][c]);
+    dfs(r, c - 1, visit, heights[r][c]);
+  };
+
+  for (let c = 0; c < cols; c++) {
+    dfs(0, c, pacMap, heights[0][c]); //Top
+    dfs(rows - 1, c, atlMap, heights[rows - 1][c]); //Bottom
   }
 
-  return result;
+  for (let r = 0; r < rows; r++) {
+    dfs(r, 0, pacMap, heights[r][0]); //Left
+    dfs(r, cols - 1, atlMap, heights[r][cols - 1]); //Right
+  }
+
+  for (let [key, cell] of atlMap) {
+    if (pacMap.has(key)) res.push(cell);
+  }
+
+  return res;
 };
-
-function dfs(matrix, row, col, previous, ocean) {
-  // if we're looking at a valid matrix position
-  if (!isValid(matrix, row, col)) return;
-
-  // ocean can't reach
-  if (matrix[row][col] < previous) return;
-
-  // the ocean was already here
-  if (ocean[row][col] === 1) return;
-
-  ocean[row][col] = 1;
-  dfs(matrix, row + 1, col, matrix[row][col], ocean);
-  dfs(matrix, row - 1, col, matrix[row][col], ocean);
-  dfs(matrix, row, col + 1, matrix[row][col], ocean);
-  dfs(matrix, row, col - 1, matrix[row][col], ocean);
-}
-
-function isValid(matrix, row, col) {
-  const rowIsValid = row >= 0 && row < matrix.length;
-  const colIsValid = col >= 0 && col < matrix[0].length;
-  return rowIsValid && colIsValid;
-}
